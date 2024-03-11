394c82e1c5015e48c40f8178704c5750984f7981
iftakhar <iftakharalamshuvo11@gmail.com>
2024-03-12 00:33:52 +0600

    some fixes

--- src/assets/Components/Bootle/Bootle.jsx
+++ src/assets/Components/Bootle/Bootle.jsx
@@ -1,15 +1,16 @@
 
 import './Bootle.css'
-const Bootle = ({bootle}) => {
+const Bootle = ({bootle,handleAddToCart}) => {
     console.log(bootle);
     const {name,img,price,seller,stock}=bootle
     return (
         <div className='bootle-container'>
             <h2>name: {name}</h2>
-            <img src={img} alt="" srcset="" />
+            <img src={img} alt="" />
             <p>Price : ${price}</p>
             <p>Seller:{seller}</p>
             <p>Stock: {stock}</p>
+            <button onClick={()=>handleAddToCart(bootle)}>Add to Cart</button>
         </div>
     );
 };
--- src/assets/Components/Bootles/Bootles.jsx
+++ src/assets/Components/Bootles/Bootles.jsx
@@ -4,20 +4,30 @@ import './Bootles.css'
 
 
 const Bootles = () => {
-    const [bootles,setBootles]=useState([])
+    const [bootles,setBootles]=useState([]);
+    const [shopingCart,setShopingCart]=useState([])
    useEffect(()=>{
     fetch('bootle.json')
     .then(res=>res.json())
     .then(data=> setBootles(data))
 },[])
+const handleAddToCart=(bootle)=>{
+  console.log(bootle);
+  const newShoppingCart=[...shopingCart,bootle];
+  setShopingCart(newShoppingCart);
+
+}
     return (
         <div>
-            <h3>Bootles Here : {bootles.length}</h3>
+            <h3>Bootles Bootle Here : {bootles.length}</h3>
+            <h4>Cart: {shopingCart.length}</h4>
           <div className="single-bottle-container">
           {
             bootles.map(bootle=><Bootle
                  key={bootle.id} 
-                 bootle={bootle}>
+                 bootle={bootle}
+                 handleAddToCart={handleAddToCart}
+                 >
                     
                  </Bootle>)
            }
